e0a1e27a40c6f9bd6bbcdfa4ab2c237905387473
Simon Walker <s.r.walker101@googlemail.com>
2015-08-08 20:14:37 +0100

    Add readline history support for prompts

--- setup.py
+++ setup.py
@@ -4,6 +4,18 @@
 from __future__ import division, print_function, absolute_import
 import argparse
 import logging
+import readline
+import os
+# Set up readline for nicer prompt work
+histfile = os.path.join(os.path.expanduser('~'), '.pipeline-install-hist')
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+import atexit
+atexit.register(readline.write_history_file, histfile)
+del histfile
+
 
 logging.basicConfig(
     level='INFO', format='%(levelname)7s %(message)s')
