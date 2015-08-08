6ccba96d317705d29b8126c34dca83aea9456efb
Simon Walker <s.r.walker101@googlemail.com>
2015-08-08 20:15:04 +0100

    Add nice prompt support

--- setup.py
+++ setup.py
@@ -16,10 +16,41 @@ import atexit
 atexit.register(readline.write_history_file, histfile)
 del histfile
 
+# Python 3
+if sys.version_info.major == 3:
+    raw_prompt = input
+else:
+    raw_prompt = raw_input
+
+class PrintInColor(object):
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    LIGHT_PURPLE = '\033[94m'
+    PURPLE = '\033[95m'
+    END = '\033[0m'
+
+    @classmethod
+    def error(cls, s):
+        return cls.RED + s + cls.END
+
+    @classmethod
+    def status(cls, s):
+        return cls.GREEN + s + cls.END
 
 logging.basicConfig(
     level='INFO', format='%(levelname)7s %(message)s')
 logger = logging.getLogger(__name__)
+
+def prompt(question, answers):
+    if question[-1] != ' ':
+        question = question + ' '
+
+    return raw_prompt(question)
+
+def yesno(question, answers={'y', 'yes', ''}):
+    answer = prompt(question, answers)
+    return answer.lower() in answers
 
 
 def main(args):
