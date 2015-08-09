73d1a6685eae38818d9d78ec8b9a851687da8e7f
Simon Walker <s.r.walker101@googlemail.com>
2015-08-09 13:00:10 +0100

    Logging messages in test script

--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@ set -e
 vagrant up
 vagrant provision
 for file in setup.py requirements*.txt; do
+    echo Uploading file $file
     cat $file | vagrant ssh -- "cat > /pipeline/$file"
 done
+echo Running test
 vagrant ssh -- -t 'cd /pipeline && python setup.py -v'
