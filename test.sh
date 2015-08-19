ee84d40230244a6be1ba3e4fa43d9a7d84f016f3
Simon Walker <s.r.walker101@googlemail.com>
2015-08-19 13:48:39 +0100

    test script requires box argument

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
 
-set -e
+set -eu
 
-vagrant up
-vagrant provision
-cat setup.py | vagrant ssh -- "cat > /pipeline/setup.py"
+if [[ ! "$#" == 1 ]]; then
+    echo "Program usage: $0 (centos|debian)" >&2
+    exit 1
+fi
+
+box="$1"
+
+vagrant up "${box}"
+vagrant provision "${box}"
+cat setup.py | vagrant ssh "${box}" -- "cat > /pipeline/setup.py"
 echo Running test
-vagrant ssh -- -t 'cd /pipeline && time python setup.py -v'
+vagrant ssh "${box}" -- -t 'cd /pipeline && time python setup.py -v'
